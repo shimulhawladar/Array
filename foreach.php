aef3fcff645062d8c18a5aff5de960dc80208581
Shimul Hawladar <55784324+shimulhawladar@users.noreply.github.com>
2019-11-21 00:33:39 -0500

    Update foreach.php

--- foreach.php
+++ foreach.php
@@ -89,31 +89,4 @@ while($i<count($array))
 */
 
 
-
-
-
-
-
-
-
-
-
-// $a = [1,2,5,6,4];
-// $b = [3,7,10,9,8];
-
-// $ab = array_merge($a,$b);
-
-
-// //print_r( $ab );
-
-// echo "<br>";
-
-// sort($ab);
-// $count = count($ab);
-
-// for($x = 0; $x<$count; $x++){
-
-// 	echo $ab[$x];
-// }
-
 ?>
